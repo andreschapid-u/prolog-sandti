228422a89a75e163b7805f2ee8efa436f2453ac4
Carlos Andres Chapid Inga <carloschapid@unicauca.edu.co>
2018-05-11 07:41:11 -0500

    Ordenacion de hechos alfabeticamente por el primer elemento

--- Proyecto.pl
+++ Proyecto.pl
@@ -1,3 +1,4 @@
+%   HECHOS DE LOS DEPARTAMENTOS DE COLOMBIA
 departamento(amazonas).
 departamento(antioquia).
 departamento(arauca).
@@ -31,62 +32,75 @@ departamento(valle_Cauca).
 departamento(vaupes).
 departamento(vichada).
 
-producto(cafe).
+%   HECHOS DE LOS PRODUCTOS
 producto(algodon).
-producto(cereales).
-producto(papa).
-producto(leguminosas).
 producto(cacao).
+producto(cafe).
+producto(cereales).
 producto(frutales).
+producto(leguminosas).
+producto(papa).
 
-cultivar(atlantico, algodon).
-cultivar(putumayo, leguminosas).
-cultivar(putumayo, frutales).
-cultivar(nari�o, cereales).
-cultivar(nari�o, frutales).
-cultivar(nari�o, papa).
-cultivar(nari�o, cafe).
-cultivar(cauca, papa).
-cultivar(cauca, frutales).
-cultivar(cauca, cereales).
-cultivar(cauca, cafe).
-cultivar(caqueta, cereales).
-cultivar(caqueta, algodon).
-cultivar(huila, trigo).
-cultivar(huila, cacao).
-cultivar(huila, cafe).
-cultivar(valle_Cauca, cacao).
-cultivar(valle_Cauca, cafe).
-cultivar(tolima, algodon).
-cultivar(tolima, leguminosas).
-cultivar(tolima, cafe).
-cultivar(tolima, cacao).
-cultivar(quindio, algodon).
-cultivar(risaralda, cereales).
-cultivar(caldas, cacao).
-cultivar(caldas, algodon).
-cultivar(caldas, cereales).
-cultivar(meta, algodon).
-cultivar(meta, cereales).
-cultivar(meta, cafe).
-cultivar(meta, frutales).
-cultivar(casanare, frutales).
-cultivar(casanare, cafe).
-cultivar(casanare, cereales).
-cultivar(casanare, algodon).
+%   PRODUCTOS QUE SE PUEDEN CULTIVAR EN UN DEPARTAMENTO
+cultivar(antioquia, cacao).
+cultivar(antioquia, leguminosas).
+cultivar(antioquia, cafe).
+cultivar(antioquia, algodon).
+cultivar(antioquia, frutales).
+cultivar(antioquia, cereales).
 cultivar(arauca, cereales).
-cultivar(cundinamarca, frutales).
-cultivar(cundinamarca, cafe).
-cultivar(cundinamarca, algodon).
-cultivar(cundinamarca, cacao).
-cultivar(cundinamarca, papa).
+cultivar(atlantico, algodon).
+cultivar(bolivar, cereales).
+cultivar(bolivar, algodon).
 cultivar(boyaca, cacao).
 cultivar(boyaca, papa).
 cultivar(boyaca, cereales).
 cultivar(boyaca, frutales).
 cultivar(boyaca, leguminosas).
-cultivar(norte_Santander, cereales).
+cultivar(caqueta, cereales).
+cultivar(caqueta, algodon).
+cultivar(caldas, cacao).
+cultivar(caldas, algodon).
+cultivar(caldas, cereales).
+cultivar(casanare, frutales).
+cultivar(casanare, cafe).
+cultivar(casanare, cereales).
+cultivar(casanare, algodon).
+cultivar(cauca, papa).
+cultivar(cauca, frutales).
+cultivar(cauca, cereales).
+cultivar(cauca, cafe).
+cultivar(cesar, cereales).
+cultivar(cesar, algodon).
+cultivar(cesar, frutales).
+cultivar(cordoba, cereales).
+cultivar(cordoba, algodon).
+cultivar(cordoba, cafe).
+cultivar(cundinamarca, frutales).
+cultivar(cundinamarca, cafe).
+cultivar(cundinamarca, algodon).
+cultivar(cundinamarca, cacao).
+cultivar(cundinamarca, papa).
+cultivar(huila, trigo).
+cultivar(huila, cacao).
+cultivar(huila, cafe).
+cultivar(magdalena, algodon).
+cultivar(magdalena, cereales).
+cultivar(magdalena, cafe).
+cultivar(meta, algodon).
+cultivar(meta, cereales).
+cultivar(meta, cafe).
+cultivar(meta, frutales).
+cultivar(nari�o, cereales).
+cultivar(nari�o, frutales).
+cultivar(nari�o, papa).
+cultivar(nari�o, cafe).
 cultivar(norte_Santander, algodon).
+cultivar(norte_Santander, cereales).
+cultivar(putumayo, leguminosas).
+cultivar(putumayo, frutales).
+cultivar(quindio, algodon).
+cultivar(risaralda, cereales).
 cultivar(santander, papa).
 cultivar(santander, cafe).
 cultivar(santander, frutales).
@@ -94,25 +108,14 @@ cultivar(santander, cereales).
 cultivar(sucre, cereales).
 cultivar(sucre, algodon).
 cultivar(sucre, cereales).
-cultivar(cesar, cereales).
-cultivar(cesar, algodon).
-cultivar(cesar, frutales).
-cultivar(cordoba, cereales).
-cultivar(cordoba, algodon).
-cultivar(cordoba, cafe).
-cultivar(antioquia, cacao).
-cultivar(antioquia, leguminosas).
-cultivar(antioquia, cafe).
-cultivar(antioquia, algodon).
-cultivar(antioquia, frutales).
-cultivar(antioquia, cereales).
-cultivar(bolivar, cereales).
-cultivar(bolivar, algodon).
-cultivar(magdalena, algodon).
-cultivar(magdalena, cereales).
-cultivar(magdalena, cafe).
+cultivar(tolima, algodon).
+cultivar(tolima, leguminosas).
+cultivar(tolima, cafe).
+cultivar(tolima, cacao).
+cultivar(valle_Cauca, cacao).
+cultivar(valle_Cauca, cafe).
 
-
+%   EJEMPLOS DE TERRENOS 
 % terreno( DEPARTAMENTO , HUMEDAD , PH , TEXTURA ).
 %   Arenosa     Arcillosa
 terreno(antioquia,50,7,arcillosa).
@@ -131,7 +134,8 @@ terreno(sucre,50,7,arcillosa).
 terreno(sucre,50,7,arenosa).
 terreno(tolima,50,7,arcillosa).
 terreno(tolima,50,7,arenosa).
-terreno(t,50,7,arenosa).
+terreno(t,50,7,arenosa). % EJEMPLO
 
+%   PRODUCTO POR TIPO DE SUELO
 
 
